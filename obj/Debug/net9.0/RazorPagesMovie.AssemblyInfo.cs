0fc8cfcb401497ddd419e3d269f210235d15941b
Gabriel Souza Farias <gabrielsfarias@outlook.com>
2025-04-16 06:07:31 -0300

    inicial2

--- obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28de339816b5187a0f5acd5999ffea958bdc2b19")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
